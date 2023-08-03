a48a3af57ab70a34f85a23cdd8f6c17b14d74261
Your Name <ospanovasofya@gmail.com>
2023-08-03 18:43:54 +0600

    commit

--- funcs/functions.go
+++ funcs/functions.go
@@ -18,13 +18,13 @@ func NewMessege(msg string, conn net.Conn, cl Client, time string) Message {
 	}
 }
 
-func Welcom(conn net.Conn) {
+func Welcome(conn net.Conn) {
 	file, err := os.ReadFile("logo.txt")
 	if err != nil {
 		fmt.Printf("couldn't read this file")
 	}
 	imagetxt := string(file)
-	conn.Write([]byte("Welcom to TCP-Chat!\n" + imagetxt + "\n"))
+	conn.Write([]byte("Welcome to TCP-Chat!\n" + imagetxt + "\n"))
 }
 
 func GetName(conn net.Conn) string {
@@ -37,13 +37,13 @@ func GetName(conn net.Conn) string {
 	temp := strings.TrimSpace(string(data))
 
 	if temp == "" || len(temp) == 0 || len(temp) > 20 {
-		fmt.Fprintln(conn, "Incorrect input")
+		fmt.Fprintln(conn, "Error: Incorrect input")
 		return GetName(conn)
 	}
 
 	for _, i := range temp {
 		if i < 32 && i > 127 {
-			fmt.Fprintln(conn, "Incorrect input\n")
+			fmt.Fprintln(conn, "Error: Incorrect input\n")
 			return GetName(conn)
 		}
 	}
@@ -56,7 +56,6 @@ func GetName(conn net.Conn) string {
 	return temp
 }
 
-// функция правильности ввода каждого подключенного к серверу пользователя
 func IsCorrect(s string, conn net.Conn, time string, username string) bool {
 	for _, w := range s {
 		if w < 32 || w > 127 {
--- funcs/handel.go
+++ funcs/handel.go
@@ -8,8 +8,8 @@ import (
 )
 
 func HandelConn(conn net.Conn) {
-	timeClient := time.Now().Format("2006-01-02 15:04:05")
-	Welcom(conn)
+	timeClient := time.Now().Format("2023-08-02 17:05:00")
+	Welcome(conn)
 	nickname := GetName(conn)
 	tempClient := Client{
 		name: nickname,
@@ -20,7 +20,7 @@ func HandelConn(conn net.Conn) {
 	clients[nickname] = tempClient
 	if len(clients) > 10 {
 		clientMutex.Lock()
-		tempClient.conn.Write([]byte("Chat is full!"))
+		tempClient.conn.Write([]byte("Error: Chat is full!"))
 		delete(clients, nickname)
 		tempClient.conn.Close()
 		clientMutex.Unlock()
@@ -28,20 +28,20 @@ func HandelConn(conn net.Conn) {
 	}
 	clientMutex.Unlock()
 	clientMutex.Lock()
-	joinChannel  <- NewMessege("has joined our chat...", conn, tempClient, timeClient)
+	joinChannel  <- NewMessege("has entered the chat...", conn, tempClient, timeClient)
 	clientMutex.Unlock()
 	fmt.Fprintf(conn, "[%s][%s]:", timeClient, nickname)
 	input := bufio.NewScanner(conn)
 
 	for input.Scan() {
-		time := time.Now().Format("2006-01-02 15:04:05")
+		time := time.Now().Format("2023-08-02 17:05:00")
 		if input.Text() == "" {
-			fmt.Fprintf(conn, "you can't send empty messages\n")
+			fmt.Fprintf(conn, "Error: Message cannot be blank.\n")
 			fmt.Fprintf(conn, "[%s][%s]:", time, nickname)
 			continue
 		}
 		if IsCorrect(input.Text(), conn, time, nickname) == false {
-			fmt.Fprintf(conn, "Incorrext inptu\n")
+			fmt.Fprintf(conn, "Error: Incorrect input.\\n")
 			fmt.Fprintf(conn, "[%s][%s]:", time, nickname)
 			continue
 		}
