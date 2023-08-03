1e16a9ee378faadc932e5488d612c0e5a9195bba
Your Name <ospanovasofya@gmail.com>
2023-08-03 18:27:39 +0600

    commit

--- funcs/server.go
+++ funcs/server.go
@@ -8,14 +8,14 @@ import (
 var conn_type = "tcp"
 
 func CreateServerPort(port string) {
-	ls, err := net.Listen(conn_type, port)
+	listener, err := net.Listen(conn_type, port)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Listening on the port: %s\n", port)
 	go Broadcaster()
 	for {
-		conn, err := ls.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
